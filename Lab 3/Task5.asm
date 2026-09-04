166e33547f00a08da0cfd9be076f786028deef3a
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-09-04 17:39:15 +0500

    Add Task5.asm with data and main procedure

---
+++ Lab 3/Task5.asm
@@ -0,0 +1,13 @@
+Include irvine32.inc
+.data
+	marks BYTE 5, 5, 5, 5, 5, 5, 5, 5
+	word1 WORD ?
+	word2 WORD ?
+	word3 WORD ?
+	word4 WORD ?
+	word5 WORD ?
+.code
+main PROC
+	exit
+main endp
+end main
